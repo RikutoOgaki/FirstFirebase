6fbb6a65e99d9c1c0b809b08ff2b792ccd822992
Rikuto <orikutobasuke@gmail.com>
2023-10-23 14:26:53 +0900

    firabaseに接続完了

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { initFirabaseApp } from '@/lib/firebase/firebase'
+import { getApp } from 'firebase/app'
+
+initFirabaseApp()
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log(getApp());
+
   return <Component {...pageProps} />
 }
